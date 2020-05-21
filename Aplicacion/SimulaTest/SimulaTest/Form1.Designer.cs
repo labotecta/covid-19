e37f41de7e7528447af171902da540bfcf66a295
labotecta <45360153+labotecta@users.noreply.github.com>
2020-05-21 19:23:19 +0200

    Aplicación para simular test

---
+++ Aplicacion/SimulaTest/SimulaTest/Form1.Designer.cs
@@ -0,0 +1,380 @@
+﻿namespace SimulaTest
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label51 = new System.Windows.Forms.Label();
+            this.prevalencia = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.sensibilidad = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.especificidad = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.simulaciones = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.ancho = new System.Windows.Forms.TextBox();
+            this.b_calcula = new System.Windows.Forms.Button();
+            this.pv = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.pf = new System.Windows.Forms.Label();
+            this.nv = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.nf = new System.Windows.Forms.Label();
+            this.positivos = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.contador = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.muestra = new System.Windows.Forms.TextBox();
+            this.grafico = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.grafico)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label51
+            // 
+            this.label51.AutoSize = true;
+            this.label51.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.label51.Location = new System.Drawing.Point(24, 23);
+            this.label51.Name = "label51";
+            this.label51.Size = new System.Drawing.Size(82, 17);
+            this.label51.TabIndex = 292;
+            this.label51.Text = "Prevalencia";
+            // 
+            // prevalencia
+            // 
+            this.prevalencia.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.prevalencia.Location = new System.Drawing.Point(172, 23);
+            this.prevalencia.Name = "prevalencia";
+            this.prevalencia.Size = new System.Drawing.Size(56, 22);
+            this.prevalencia.TabIndex = 291;
+            this.prevalencia.Text = "5";
+            this.prevalencia.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.label1.Location = new System.Drawing.Point(24, 49);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(84, 17);
+            this.label1.TabIndex = 294;
+            this.label1.Text = "Sensibilidad";
+            // 
+            // sensibilidad
+            // 
+            this.sensibilidad.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.sensibilidad.Location = new System.Drawing.Point(172, 49);
+            this.sensibilidad.Name = "sensibilidad";
+            this.sensibilidad.Size = new System.Drawing.Size(56, 22);
+            this.sensibilidad.TabIndex = 293;
+            this.sensibilidad.Text = "97";
+            this.sensibilidad.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.label2.Location = new System.Drawing.Point(24, 76);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(81, 17);
+            this.label2.TabIndex = 296;
+            this.label2.Text = "Especifidad";
+            // 
+            // especificidad
+            // 
+            this.especificidad.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.especificidad.Location = new System.Drawing.Point(172, 76);
+            this.especificidad.Name = "especificidad";
+            this.especificidad.Size = new System.Drawing.Size(56, 22);
+            this.especificidad.TabIndex = 295;
+            this.especificidad.Text = "100";
+            this.especificidad.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.label3.Location = new System.Drawing.Point(253, 23);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(91, 17);
+            this.label3.TabIndex = 298;
+            this.label3.Text = "Simulaciones";
+            // 
+            // simulaciones
+            // 
+            this.simulaciones.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.simulaciones.Location = new System.Drawing.Point(401, 23);
+            this.simulaciones.Name = "simulaciones";
+            this.simulaciones.Size = new System.Drawing.Size(56, 22);
+            this.simulaciones.TabIndex = 297;
+            this.simulaciones.Text = "100000";
+            this.simulaciones.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.label4.Location = new System.Drawing.Point(253, 49);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(59, 17);
+            this.label4.TabIndex = 300;
+            this.label4.Text = "Muestra";
+            // 
+            // ancho
+            // 
+            this.ancho.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.ancho.Location = new System.Drawing.Point(401, 76);
+            this.ancho.Name = "ancho";
+            this.ancho.Size = new System.Drawing.Size(56, 22);
+            this.ancho.TabIndex = 299;
+            this.ancho.Text = "0,1";
+            this.ancho.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // b_calcula
+            // 
+            this.b_calcula.Location = new System.Drawing.Point(289, 108);
+            this.b_calcula.Margin = new System.Windows.Forms.Padding(2);
+            this.b_calcula.Name = "b_calcula";
+            this.b_calcula.Size = new System.Drawing.Size(168, 29);
+            this.b_calcula.TabIndex = 301;
+            this.b_calcula.Text = "Calcula";
+            this.b_calcula.UseVisualStyleBackColor = true;
+            this.b_calcula.Click += new System.EventHandler(this.B_calcula_Click);
+            // 
+            // pv
+            // 
+            this.pv.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pv.Location = new System.Drawing.Point(200, 190);
+            this.pv.Name = "pv";
+            this.pv.Size = new System.Drawing.Size(42, 22);
+            this.pv.TabIndex = 330;
+            this.pv.Text = "0";
+            this.pv.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.label5.Location = new System.Drawing.Point(102, 190);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(56, 17);
+            this.label5.TabIndex = 331;
+            this.label5.Text = "TEST +";
+            // 
+            // label6
+            // 
+            this.label6.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.label6.Location = new System.Drawing.Point(200, 162);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(45, 17);
+            this.label6.TabIndex = 332;
+            this.label6.Text = "SI";
+            this.label6.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label7
+            // 
+            this.label7.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.label7.Location = new System.Drawing.Point(283, 162);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(45, 17);
+            this.label7.TabIndex = 333;
+            this.label7.Text = "NO";
+            this.label7.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // pf
+            // 
+            this.pf.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pf.Location = new System.Drawing.Point(283, 190);
+            this.pf.Name = "pf";
+            this.pf.Size = new System.Drawing.Size(42, 22);
+            this.pf.TabIndex = 334;
+            this.pf.Text = "0";
+            this.pf.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // nv
+            // 
+            this.nv.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.nv.Location = new System.Drawing.Point(283, 216);
+            this.nv.Name = "nv";
+            this.nv.Size = new System.Drawing.Size(42, 22);
+            this.nv.TabIndex = 337;
+            this.nv.Text = "0";
+            this.nv.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.label9.Location = new System.Drawing.Point(102, 216);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(53, 17);
+            this.label9.TabIndex = 336;
+            this.label9.Text = "TEST -";
+            // 
+            // nf
+            // 
+            this.nf.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.nf.Location = new System.Drawing.Point(200, 216);
+            this.nf.Name = "nf";
+            this.nf.Size = new System.Drawing.Size(42, 22);
+            this.nf.TabIndex = 335;
+            this.nf.Text = "0";
+            this.nf.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // positivos
+            // 
+            this.positivos.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.positivos.Location = new System.Drawing.Point(377, 190);
+            this.positivos.Name = "positivos";
+            this.positivos.Size = new System.Drawing.Size(42, 22);
+            this.positivos.TabIndex = 338;
+            this.positivos.Text = "0";
+            this.positivos.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // label10
+            // 
+            this.label10.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.label10.Location = new System.Drawing.Point(345, 162);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(74, 17);
+            this.label10.TabIndex = 339;
+            this.label10.Text = "Positivos";
+            this.label10.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // contador
+            // 
+            this.contador.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.contador.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.contador.Location = new System.Drawing.Point(51, 162);
+            this.contador.Name = "contador";
+            this.contador.Size = new System.Drawing.Size(125, 22);
+            this.contador.TabIndex = 340;
+            this.contador.Text = "0";
+            this.contador.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.label8.Location = new System.Drawing.Point(253, 76);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(122, 17);
+            this.label8.TabIndex = 342;
+            this.label8.Text = "Ancho histograma";
+            // 
+            // muestra
+            // 
+            this.muestra.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.muestra.Location = new System.Drawing.Point(401, 49);
+            this.muestra.Name = "muestra";
+            this.muestra.Size = new System.Drawing.Size(56, 22);
+            this.muestra.TabIndex = 341;
+            this.muestra.Text = "50000";
+            this.muestra.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // grafico
+            // 
+            this.grafico.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.grafico.Location = new System.Drawing.Point(18, 253);
+            this.grafico.Name = "grafico";
+            this.grafico.Size = new System.Drawing.Size(447, 349);
+            this.grafico.TabIndex = 343;
+            this.grafico.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(486, 617);
+            this.Controls.Add(this.grafico);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.muestra);
+            this.Controls.Add(this.contador);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.positivos);
+            this.Controls.Add(this.nv);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.nf);
+            this.Controls.Add(this.pf);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.pv);
+            this.Controls.Add(this.b_calcula);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.ancho);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.simulaciones);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.especificidad);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.sensibilidad);
+            this.Controls.Add(this.label51);
+            this.Controls.Add(this.prevalencia);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Simula test";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.grafico)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label51;
+        private System.Windows.Forms.TextBox prevalencia;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox sensibilidad;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox especificidad;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox simulaciones;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox ancho;
+        private System.Windows.Forms.Button b_calcula;
+        private System.Windows.Forms.Label pv;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label pf;
+        private System.Windows.Forms.Label nv;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label nf;
+        private System.Windows.Forms.Label positivos;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label contador;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox muestra;
+        private System.Windows.Forms.PictureBox grafico;
+    }
+}
+
---
+++ Aplicacion/SimulaTest/SimulaTest/Form1.cs
@@ -0,0 +1,218 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace SimulaTest
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void B_calcula_Click(object sender, EventArgs e)
+        {
+            Calcular();
+        }
+        private void Calcular()
+        {
+            double pre = Convert.ToDouble(prevalencia.Text) / 100;
+            double sen = Convert.ToDouble(sensibilidad.Text) / 100;
+            double espe = Convert.ToDouble(especificidad.Text) / 100;
+            long num_simulaciones = Convert.ToInt32(simulaciones.Text);
+            long num_muestra = Convert.ToInt32(muestra.Text);
+            double ancho_his = Convert.ToDouble(ancho.Text.Replace('.', ','));
+            int n_histo = (int)(100.0 / ancho_his) + 1;
+            long[] histo = new long[n_histo];
+            int ih;
+            int max_ih = -1;
+            int min_ih = int.MaxValue;
+            long pv_cm;
+            long pf_cm;
+            long nv_cm;
+            long nf_cm;
+            long pv_c = 0;
+            long pf_c = 0;
+            long nv_c = 0;
+            long nf_c = 0;
+            long tt = 0;
+            pv.Text = string.Format("{0:N0}", pv_c);
+            pf.Text = string.Format("{0:N0}", pf_c);
+            nv.Text = string.Format("{0:N0}", nv_c);
+            nf.Text = string.Format("{0:N0}", nf_c);
+            positivos.Text = string.Format("{0:N0}", pv_c + pf_c);
+            Random azar;
+            azar = new Random(12345);
+            bool pre_real;
+            double pm;
+            for (long i = 0; i < num_simulaciones; i++)
+            {
+                pv_cm = 0;
+                pf_cm = 0;
+                nv_cm = 0;
+                nf_cm = 0;
+                for (long j = 0; j < num_muestra; j++)
+                {
+                    // Prevalencia real
+
+                    pre_real = azar.NextDouble() < pre ? true : false;
+                    if (pre_real)
+                    {
+                        // Sensibilidad
+
+                        if (azar.NextDouble() < sen)
+                        {
+                            // Positivo verdadero
+
+                            pv_cm++;
+                        }
+                        else
+                        {
+                            // Negativo falso (pre_real es true)
+
+                            nf_cm++;
+                        }
+                    }
+                    else
+                    {
+                        // Especificidad
+
+                        if (azar.NextDouble() < espe)
+                        {
+                            nv_cm++;
+                        }
+                        else
+                        {
+                            pf_cm++;
+                        }
+                    }
+                }
+                pv_c += pv_cm;
+                pf_c += pf_cm;
+                nv_c += nv_cm;
+                nf_c += nf_cm;
+                pm = 100.0 * (pv_cm + pf_cm) / num_muestra;
+                ih = (int)(pm / ancho_his);
+                histo[ih]++;
+                if (ih < min_ih) min_ih = ih;
+                if (ih > max_ih) max_ih = ih;
+                if (i % 100 == 0)
+                {
+                    contador.Text = string.Format("{0:N0}", i + 1);
+                    pv.Text = string.Format("{0:f2}", 100.0 * pv_cm / num_muestra);
+                    pf.Text = string.Format("{0:f2}", 100.0 * pf_cm / num_muestra);
+                    nv.Text = string.Format("{0:f2}", 100.0 * nv_cm / num_muestra);
+                    nf.Text = string.Format("{0:f2}", 100.0 * nf_cm / num_muestra);
+                    positivos.Text = string.Format("{0:f2}", pm);
+                    Grafico(histo, min_ih, max_ih, ancho_his, i);
+                    Application.DoEvents();
+                }
+                tt += num_muestra;
+            }
+            contador.Text = string.Format("{0:N0}", num_simulaciones);
+            pv.Text = string.Format("{0:f2}", 100.0 * pv_c / tt);
+            pf.Text = string.Format("{0:f2}", 100.0 * pf_c / tt);
+            nv.Text = string.Format("{0:f2}", 100.0 * nv_c / tt);
+            nf.Text = string.Format("{0:f2}", 100.0 * nf_c / tt);
+            pm = 100.0 * (pv_c + pf_c) / tt;
+            positivos.Text = string.Format("{0:f2}", pm);
+            Application.DoEvents();
+            if (!Directory.Exists(@"C:\Contagio")) Directory.CreateDirectory(@"C:\Contagio");
+            string F_CASO = string.Format(@"C:\Contagio\Probailidad_test_{0}_{1}_{2}.csv", prevalencia.Text, sensibilidad.Text, especificidad.Text);
+            FileStream fw = new FileStream(F_CASO, FileMode.Create, FileAccess.Write, FileShare.Read);
+            StreamWriter sw = new StreamWriter(fw);
+            long v_max = -1;
+            max_ih++;
+            for (int i = 0; i < max_ih; i++)
+            {
+                if (v_max < histo[i]) v_max = histo[i];
+            }
+            sw.WriteLine("{0:f3};", 100.0 * v_max / num_simulaciones);
+            double x = 0;
+            for (int i = 0; i < max_ih; i++)
+            {
+                sw.WriteLine("{0:f3};{1:f3}", x + ancho_his / 2, 100.0 * histo[i] / num_simulaciones);
+                x += ancho_his;
+            }
+            sw.Close();
+            MessageBox.Show("Simulación terminada");
+            Console.Beep();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            grafico.Image = null;
+            b_calcula.Focus();
+        }
+        private void Grafico(long[] histo, int min_ih, int max_ih, double ancho_his, long num_simulaciones)
+        {
+            Bitmap img_grafico = new Bitmap(grafico.Width, grafico.Height);
+            Graphics g = Graphics.FromImage(img_grafico);
+            Brush brocha = Brushes.White;
+            g.FillRectangle(brocha, 0, 0, grafico.Width, grafico.Height);
+            int nRE = histo.Length;
+            double medio_ancho = ancho_his / 2;
+            double[] x = new double[nRE];
+            double[] y = new double[nRE];
+            double xmin = min_ih * ancho_his + medio_ancho;
+            double xmax = max_ih * ancho_his + medio_ancho;
+            double ymin = double.MaxValue;
+            double ymax = double.MinValue;
+            int imy = -1;
+            for (int i = min_ih; i < max_ih; i++)
+            {
+                x[i] = i * ancho_his + medio_ancho;
+                y[i] = 100.0 * histo[i] / (num_simulaciones + 1);
+                if (y[i] < ymin) ymin = y[i];
+                if (y[i] > ymax)
+                {
+                    imy = i;
+                    ymax = y[i];
+                }
+            }
+            if (imy == -1) return;
+            Brush azul = Brushes.Blue;
+            int margen = 60;
+            int mediomargen = margen / 2;
+            double fx = (grafico.Width - margen) / (xmax - xmin);
+            double fy;
+            if (ymin == double.MaxValue || ymax == double.MinValue || ymax == ymin)
+            {
+                fy = 1.0d;
+            }
+            else
+            {
+                fy = (grafico.Height - margen) / (ymax - ymin);
+            }
+            int xi;
+            int an = (int)(fx * ancho_his);
+            int al;
+            int yi;
+            for (int i = min_ih; i < max_ih; i++)
+            {
+                xi = mediomargen + (int)(fx * (x[i] - xmin));
+                al = (int)(fy * (y[i] - ymin));
+                yi = grafico.Height - mediomargen - al;
+                if (y[i] != -1) g.FillRectangle(azul, xi, yi, an, al);
+            }
+            Font fuente = new Font("Arial", 10);
+            string s = string.Format("{0:f2}", xmin);
+            g.DrawString(s, fuente, Brushes.Black, 0, grafico.Height - 20);
+            s = string.Format("{0:f2}", x[imy]);
+            g.DrawString(s, fuente, Brushes.Black, mediomargen + (int)(fx * (x[imy] - xmin)), grafico.Height - 20);
+            s = string.Format("{0:f2}", xmax);
+            g.DrawString(s, fuente, Brushes.Black, grafico.Width - 44, grafico.Height - 20);
+            s = string.Format("{0:f2}", ymax);
+            g.DrawString(s, fuente, Brushes.Black, 0, 0);
+            grafico.Image = img_grafico;
+            Application.DoEvents();
+        }
+    }
+}
